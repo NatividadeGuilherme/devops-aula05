bb100feabfdababa4bc3f4ccec843340985fbfc9
Guilherme Natividade <36283471+NatividadeGuilherme@users.noreply.github.com>
2018-03-09 21:35:01 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -9,7 +9,7 @@ if len(jogo) != 3:
     erroInicializar = True
 else:
     for linha in jogo:
-        if len(linha) != 3:
+        if len(linha) != 3;
             erroInicializar = True
         else:
             for elemento in linha:
